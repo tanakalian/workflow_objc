b7546f09371ab444d12ebc1a72c6ebbd6c45f4db
Jon Palmisciano <jp@jonpalmisc.com>
2022-06-28 16:07:59 -0400

    Resolve non-virtual destructor warning on `BinaryViewFile`

--- Core/BinaryViewFile.h
+++ Core/BinaryViewFile.h
@@ -26,6 +26,7 @@ class BinaryViewFile : public ObjectiveNinja::AbstractFile {
 
 public:
     explicit BinaryViewFile(BinaryViewRef);
+    virtual ~BinaryViewFile() {}
 
     void seek(uint64_t) override;
 
